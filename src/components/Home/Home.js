e99411a07f2e91d87a310f733ca18f2b7b5cdd6c
Luo Long <97410344+howielongs@users.noreply.github.com>
2024-09-01 13:34:37 -0700

    updated resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import Tilt from "react-parallax-tilt";
 
 function Home() {
   return (
@@ -31,12 +32,14 @@ function Home() {
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
+              <Tilt>
               <img
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
               />
+              </Tilt>
             </Col>
           </Row>
         </Container>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,9 +67,7 @@ function Home2() {
             </p>
           </Col>
           <Col md={4} className="myAvtar">
-            <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
           </Col>
         </Row>
 
