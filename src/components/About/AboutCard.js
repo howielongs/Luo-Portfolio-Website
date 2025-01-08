8a3cd4ca1a3f65f11f480fe016a6c384c4d9f114
Luo Long <97410344+howielongs@users.noreply.github.com>
2025-01-08 11:18:20 -0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,104 +11,54 @@ import image6 from "../../Assets/image6.jpg";
 import image7 from "../../Assets/image7.jpg";
 import image8 from "../../Assets/image8.jpg";
 
-
 function AboutCard() {
   return (
     <div>
       <Carousel className="about-carousel" controls={false} indicators={false} interval={2000} fade>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image1}
-            alt="Slide 1"
-          />
+          <img className="d-block w-100" src={image1} alt="Slide 1" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image2}
-            alt="Slide 2"
-          />
+          <img className="d-block w-100" src={image2} alt="Slide 2" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image3}
-            alt="Slide 3"
-          />
+          <img className="d-block w-100" src={image3} alt="Slide 3" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image4}
-            alt="Slide 4"
-          />
+          <img className="d-block w-100" src={image4} alt="Slide 4" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image5}
-            alt="Slide 5"
-          />
+          <img className="d-block w-100" src={image5} alt="Slide 5" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image6}
-            alt="Slide 6"
-          />
+          <img className="d-block w-100" src={image6} alt="Slide 6" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image7}
-            alt="Slide 7"
-          />
+          <img className="d-block w-100" src={image7} alt="Slide 7" />
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={image8}
-            alt="Slide 8"
-          />
+          <img className="d-block w-100" src={image8} alt="Slide 8" />
         </Carousel.Item>
       </Carousel>
       <Card className="quote-card-view">
         <Card.Body>
           <blockquote className="blockquote mb-0">
             <p style={{ textAlign: "justify" }}>
-              Hey everyone, my name is <span className="purple">Luo Long </span>
-              and I'm from <span className="purple"> Wilton, CT.</span>
-              <br />
-              I'm currently a senior studying <span className="purple"> Computer Science</span> at <span className="purple"> Santa Clara University</span>
-              <br />
-              I have interned as a Software Developer and Quality Assurance Intern at Undercurrent.ai and am running my own startup called{" "}
-              <span className="purple">
-                <a
-                  href="https://www.linkedin.com/company/perfect-fitt/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ color: "inherit" }}
-                >
-                  Perfect Fit
-                </a>
-              </span>
+              Hi there! My name is <span className="purple">Luo Long</span>, and I’m from <span className="purple">Wilton, CT</span>. 
+              I’m currently a senior studying <span className="purple">Computer Science</span> at 
+              <span className="purple"> Santa Clara University</span>.
               <br />
               <br />
-              Apart from coding, here are some other activities that I love to do!
+              I enjoy on connecting with people and exploring the world around me. <span className="purple">Basketball</span> has always been my universal connector, 
+              a way to bond with people no matter where I go. <span className="purple">Traveling</span> fuels my curiosity and love for new experiences, and 
+              if you have any recommendations, I’m all ears! <span className="purple">Fashion</span> is another passion of mine, a creative outlet that lets me express my individuality.
+              <br />
+              <br />
+              Growing up, I spent countless hours playing Pokémon and Super Smash Bros., which sparked my competitive spirit and love for gaming. 
+              Over time, I discovered coding as a way to express myself and solve real-world challenges. Whether it's building an <span className="purple">Outfit Generator </span>  
+              to manage my wardrobe, designing a <span className="purple">Trip Planner App</span> for my travels, or <span className="purple">Conducting Research</span> on SCU Division 1 athletes to optimize training 
+              and prevent injuries, I’m always excited to create and explore.
             </p>
-            <ul>
-              <li className="about-activity">
-                <TbPoint /> Basketball
-              </li>
-              <li className="about-activity">
-                <TbPoint /> Traveling
-              </li>
-              <li className="about-activity">
-                <TbPoint /> Fashion
-              </li>
-            </ul>
-
           </blockquote>
         </Card.Body>
       </Card>
